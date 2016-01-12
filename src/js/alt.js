ab97859bb584ea937f6de7acf227b23845a2215d
Seth <seth_wieder@condenast.com>
2016-01-12 17:06:31 -0500

    add chromeDebug to alt for DevTool

--- src/js/alt.js
+++ src/js/alt.js
@@ -1,4 +1,9 @@
+import chromeDebug from "alt-utils/lib/chromeDebug";
 var Alt = require('alt');
 var alt = new Alt();
+
+if(process.title == "browser" && location.hostname.indexOf("localhost") > -1){
+  Alt.debug = chromeDebug(alt);  
+}
 
 module.exports = alt;
